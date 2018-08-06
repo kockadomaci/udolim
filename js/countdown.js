5fd1819aed633267e82a77ce1d4a4ebfd2644ba9
kockadomaci <dita.prikrylova@greenfoxacademy.com>
2018-08-06 13:34:47 +0300

    added blog menu site

--- js/countdown.js
+++ js/countdown.js
@@ -29,10 +29,8 @@ CountdownTimer.prototype={
 }
 function CDT(){
 
- // Set countdown limit
  var tl = new Date('2018/10/1 00:00:00');
 
- // You can add time's up message here
  var timer = new CountdownTimer('CDT',tl,'<span class="number-wrapper"><div class="line"></div><span class="number end">Time is up!</span></span>');
  timer.countDown();
 }
